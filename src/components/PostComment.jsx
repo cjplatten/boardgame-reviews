19254074ac8c3769818d5bfbad83a241979e0bae
Carrie <cjplatten@gmail.com>
2021-10-21 17:51:54 +0100

    add optimistic rendering to comment posting

--- src/components/PostComment.jsx
+++ src/components/PostComment.jsx
@@ -5,11 +5,12 @@ import { UserContext } from "../contexts/UserContext";
 import { postComment } from "../utils/api";
 import { useParams } from "react-router-dom";
 
-const PostComment = ({ comments, setComments }) => {
+const PostComment = ({ comments, setComments, setNewCommentRender }) => {
   const { review_id } = useParams();
   const { userLogin } = useContext(UserContext);
   const [newComment, setNewComment] = useState({});
-  const [isPosted, setIstPosted] = useState(false);
+  const [commentResponse, setCommentResponse] = useState({});
+  const [isPosted, setIsPosted] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
 
   function handleSubmit(e) {
@@ -17,44 +18,34 @@ const PostComment = ({ comments, setComments }) => {
     setIsLoading(true);
     console.log(newComment);
     postComment(review_id, newComment, userLogin.user).then(
-      (commentResponse) => {
+      (responseFromApi) => {
+        setCommentResponse(responseFromApi);
         console.log(commentResponse);
+        setNewCommentRender((currNewCommentRender) => {
+          const updatedCommentRender = [...currNewCommentRender]
+          updatedCommentRender.unshift(responseFromApi)
+          return updatedCommentRender
+        })
+        setIsPosted(true)
       }
     );
-    setIsLoading(false)
+    setIsLoading(false);
     console.log("submitted!", e);
   }
 
   function handleInputChange(e) {
-    setNewComment(e.target.value)
+    setNewComment(e.target.value);
   }
 
   //add functionality that unshifts the new comment to a spread of the comments array once posting has finished for optimistic rendering to display the new comment at the top?
 
-  let hidePostComment = true;
-
   return (
     <>
-      {/* {hidePostComment ? (
-        <button
-          onClick={() => {
-            hidePostComment = false;
-            console.log(hidePostComment);
-          }}
-        >
-          Show Post Comment
-        </button>
-      ) : (
-        <> 
-          <button
-            onClick={() => {
-              hidePostComment = true;
-            }}
-          >
-            Hide Post Comment
-          </button>
-        */}
-      <div className="post-comment-container">
+      {isPosted ? <> 
+      <h3>Comment posted!</h3>
+      <button onClick={()=>setIsPosted(false)}>Post another comment?</button>
+      </>
+      :<div className="post-comment-container">
         <form
           onSubmit={(e) => {
             handleSubmit(e);
@@ -73,9 +64,7 @@ const PostComment = ({ comments, setComments }) => {
           <br></br>
           <button className="post-comment-button">Submit</button>
         </form>
-      </div>
-      {/* </>
-      )} */}
+      </div>}
     </>
   );
 };
--- src/components/SingleReviewComments.jsx
+++ src/components/SingleReviewComments.jsx
@@ -6,43 +6,68 @@ import PostComment from "./PostComment";
 const SingleReviewComments = () => {
   const [comments, setComments] = useState([]);
   const { review_id } = useParams();
-  const [isLoading, setIsLoading] = useState(false)
+  const [isLoading, setIsLoading] = useState(false);
+  const [newCommentRender, setNewCommentRender] = useState([]);
 
   useEffect(() => {
-    setIsLoading(true)
+    setIsLoading(true);
     getReviewComments(review_id).then((commentsFromApi) => {
       setComments(commentsFromApi);
-      setIsLoading(false)
+      setIsLoading(false);
     });
-  }, []);
+  }, [newCommentRender]);
 
   return (
     <div>
       <h2>Comments</h2>
-      <PostComment comments={comments} setComments={setComments}/>
-      {isLoading ? <h2> Loading... </h2>
-      : <ul className="all-comments">
-        {comments.map((comment, index) => {
-          return (
-            <li className="comments-in-list" key={comment.comment_id}>
-              <div className="comment-author">
-                <h4>{comment.author}</h4>
-              </div>
-              <div className="comment-info-line">
-                <h5>{comment.created_at.slice(0, 10)}</h5>
-              </div>
-              <div className="comment-body">
-                <p>{comment.body}</p>
-              </div>
-              <div className="comment-vote-counters">
-                <p>
-                  Votes: {comment.votes}
-                </p>
-              </div>
-            </li>
-          );
-        })}
-      </ul>}
+      <PostComment
+        comments={comments}
+        setComments={setComments}
+        setNewCommentRender={setNewCommentRender}
+      />
+      {isLoading ? (
+        <h2> Loading... </h2>
+      ) : (
+        <ul className="all-comments">
+          {newCommentRender &&
+            newCommentRender.map((comment, index) => {
+              return (
+                <li className="comments-in-list" key={comment.comment_id}>
+                  <div className="comment-author">
+                    <h4>{comment.author}</h4>
+                  </div>
+                  <div className="comment-info-line">
+                    <h5>{comment.created_at.slice(0, 10)}</h5>
+                  </div>
+                  <div className="comment-body">
+                    <p>{comment.body}</p>
+                  </div>
+                  <div className="comment-vote-counters">
+                    <p>Votes: {comment.votes}</p>
+                  </div>
+                </li>
+              );
+            })}
+          {comments.map((comment, index) => {
+            return (
+              <li className="comments-in-list" key={comment.comment_id}>
+                <div className="comment-author">
+                  <h4>{comment.author}</h4>
+                </div>
+                <div className="comment-info-line">
+                  <h5>{comment.created_at.slice(0, 10)}</h5>
+                </div>
+                <div className="comment-body">
+                  <p>{comment.body}</p>
+                </div>
+                <div className="comment-vote-counters">
+                  <p>Votes: {comment.votes}</p>
+                </div>
+              </li>
+            );
+          })}
+        </ul>
+      )}
     </div>
   );
 };
