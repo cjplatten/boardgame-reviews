86305f4344d90d981533c486a10cfa71d1c04d12
Carrie <cjplatten@gmail.com>
2021-11-04 00:56:20 +0000

    add some styling to Home and Single Review pages

--- src/components/PostComment.jsx
+++ src/components/PostComment.jsx
@@ -1,6 +1,6 @@
 import { useContext } from "react";
-import { useState, useEffect } from "react";
-import { Button, Form } from "react-bootstrap";
+import { useState } from "react";
+import { Form } from "react-bootstrap";
 import { UserContext } from "../contexts/UserContext";
 import { postComment } from "../utils/api";
 import { useParams } from "react-router-dom";
@@ -9,7 +9,6 @@ const PostComment = ({ comments, setComments, setNewCommentRender }) => {
   const { review_id } = useParams();
   const { userLogin } = useContext(UserContext);
   const [newComment, setNewComment] = useState({});
-  const [commentResponse, setCommentResponse] = useState({});
   const [isPosted, setIsPosted] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
 
@@ -18,13 +17,12 @@ const PostComment = ({ comments, setComments, setNewCommentRender }) => {
     setIsLoading(true);
     postComment(review_id, newComment, userLogin.user).then(
       (responseFromApi) => {
-        setCommentResponse(responseFromApi);
         setNewCommentRender((currNewCommentRender) => {
-          const updatedCommentRender = [...currNewCommentRender]
-          updatedCommentRender.unshift(responseFromApi)
-          return updatedCommentRender
-        })
-        setIsPosted(true)
+          const updatedCommentRender = [...currNewCommentRender];
+          updatedCommentRender.unshift(responseFromApi);
+          return updatedCommentRender;
+        });
+        setIsPosted(true);
       }
     );
     setIsLoading(false);
@@ -36,32 +34,39 @@ const PostComment = ({ comments, setComments, setNewCommentRender }) => {
 
   //add functionality that unshifts the new comment to a spread of the comments array once posting has finished for optimistic rendering to display the new comment at the top?
 
+  if (isLoading) return <p>Posting comment...</p>
+
   return (
     <>
-      {isPosted ? <> 
-      <h3>Comment posted!</h3>
-      <button onClick={()=>setIsPosted(false)}>Post another comment?</button>
-      </>
-      :<div className="post-comment-container">
-        <form
-          onSubmit={(e) => {
-            handleSubmit(e);
-          }}
-        >
-          <label htmlFor="post-comment">Post Comment</label>
-          <br></br>
-          <input
-            name="post-comment"
-            onChange={(e) => {
-              handleInputChange(e);
+      {isPosted ? (
+        <>
+          <p>Comment posted!</p>
+          <button className="post-another-comment-button" onClick={() => setIsPosted(false)}>
+            Post another comment?
+          </button>
+        </>
+      ) : (
+        <div className="post-comment-container">
+          <Form
+            onSubmit={(e) => {
+              handleSubmit(e);
             }}
-            required
-            // onKeyPress={(e) => { e.key === 'Enter' && e.preventDefault(); }}
-          />
-          <br></br>
-          <button className="post-comment-button">Submit</button>
-        </form>
-      </div>}
+          >
+            <Form.Label>Post Comment</Form.Label>
+            <Form.Control
+              as="textarea"
+              rows={3}
+              name="post-comment"
+              onChange={(e) => {
+                handleInputChange(e);
+              }}
+              required
+            />
+
+            <button className="post-comment-button" type="submit">Submit</button>
+          </Form>
+        </div>
+      )}
     </>
   );
 };
